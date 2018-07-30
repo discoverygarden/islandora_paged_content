1e1bbd9a0d0073f5bfa7cf427d5ebf3bba6eb1b6
willtp87 <willtp87@hotmail.com>
2018-07-30 20:16:02 -0300

    ported page progression

--- src/Form/ManagePageProgression.php
+++ src/Form/ManagePageProgression.php
@@ -20,16 +20,37 @@ class ManagePageProgression extends FormBase {
   }
 
   /**
-   * {@inheritdoc}
+   * Sets the page progression for a paged content.
    */
   public function buildForm(array $form, FormStateInterface $form_state, AbstractObject $object = NULL) {
-    return $form;
+    $form_state->loadInclude('islandora_paged_content', 'inc', 'includes/utilities');
+    $page_progression = islandora_paged_content_get_page_progression($object);
+    $form_state->setStorage(['pid' => $object->id]);
+    return [
+      'page_progression' => [
+        '#type' => 'radios',
+        '#title' => $this->t('Page Progression'),
+        '#options' => [
+          'lr' => $this->t('Left-to-Right'),
+          'rl' => $this->t('Right-to-Left'),
+        ],
+        '#default_value' => $page_progression,
+      ],
+      'submit' => [
+        '#type' => 'submit',
+        '#value' => $this->t('Set page progression'),
+      ],
+    ];
   }
 
   /**
-   * {@inheritdoc}
+   * Submit handler to set page progression for paged content.
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    module_load_include('inc', 'islandora_paged_content', 'includes/utilities');
+    $object = islandora_object_load($form_state->getStorage()['pid']);
+    islandora_paged_content_set_page_progression($object, $form_state->getValue('page_progression'));
+    drupal_set_message($this->t('Page progression updated.'));
   }
 
 }
