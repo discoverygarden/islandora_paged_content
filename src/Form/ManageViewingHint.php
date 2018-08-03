544add59f56df8a8d7a6445ec90f2ae20b4dc1a9
willtp87 <willtp87@hotmail.com>
2018-08-03 11:07:56 -0300

    porting viewing hint

--- src/Form/ManageViewingHint.php
+++ src/Form/ManageViewingHint.php
@@ -8,7 +8,7 @@ use Drupal\Core\Form\FormStateInterface;
 use AbstractObject;
 
 /**
- * {@inheritdoc}
+ * Form for viewing hint.
  */
 class ManageViewingHint extends FormBase {
 
@@ -20,16 +20,40 @@ class ManageViewingHint extends FormBase {
   }
 
   /**
-   * {@inheritdoc}
+   * Form handler for setting the Viewing mode hint for a paged content.
    */
   public function buildForm(array $form, FormStateInterface $form_state, AbstractObject $object = NULL) {
-    return $form;
+    $form_state->loadInclude('islandora_paged_content', 'inc', 'includes/utilities');
+    $viewing_hint = islandora_paged_content_get_viewing_hint($object);
+    $viewing_hint = isset($viewing_hint) ? $viewing_hint : 'notset';
+    $form_state->setStorage(['pid' => $object->id]);
+    return [
+      'viewing_hint' => [
+        '#type' => 'radios',
+        '#title' => $this->t('Page display mode'),
+        '#description' => $this->t("This affects how Islandora viewers display page sequences. 'Paged' here is equivalent to the '2-up' mode in IA Book Reader. If left as 'System Default', global option for each viewer will be respected."),
+        '#options' => [
+          'notset' => $this->t('System Default'),
+          'paged' => $this->t('Paged'),
+          'individual' => $this->t('Individual'),
+        ],
+        '#default_value' => $viewing_hint,
+      ],
+      'submit' => [
+        '#type' => 'submit',
+        '#value' => $this->t('Set Page display mode'),
+      ],
+    ];
   }
 
   /**
-   * {@inheritdoc}
+   * Submit handler for setting the Viewing mode hint for paged content.
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $object = islandora_object_load($form_state->getStorage()['pid']);
+    $viewing_hint = $form_state->getValue('viewing_hint') == 'notset' ? NULL : $form_state->getValue('viewing_hint');
+    islandora_paged_content_set_viewing_hint($object, $viewing_hint);
+    drupal_set_message($this->t('Page display mode updated.'));
   }
 
 }
