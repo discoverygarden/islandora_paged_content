ad6bf62b420862bf8f72c8ecb1539159bd52368d
willtp87 <willtp87@hotmail.com>
2019-01-17 12:11:46 -0400

    edit_ocr ported

--- js/islandora_page_ocr_edit.js
+++ js/islandora_page_ocr_edit.js
@@ -7,7 +7,11 @@
   Drupal.behaviors.islandora_paged_content = {
     attach: function (context, settings) {
       $("a.preview").click(function () {
-        $('div.ref_image').dialog(({position: { my: "left top", at: "top", collision:'none', of:'#edit-ocr' }, width: Drupal.settings.width}));
+        $('div.ref_image').dialog(
+        ({
+          position: {my: "left top", at: "top", collision:'none', of:'#edit-ocr' },
+          width: drupalSettings.islandora_paged_content.edit_ocr.width
+        }));
       });
     }
   };
--- src/Form/ManagePageEditOcr.php
+++ src/Form/ManagePageEditOcr.php
@@ -5,6 +5,9 @@ namespace Drupal\islandora_paged_content\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Url;
+
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use AbstractObject;
@@ -15,12 +18,14 @@ use AbstractObject;
 class ManagePageEditOcr extends FormBase {
 
   protected $moduleHandler;
+  protected $renderer;
 
   /**
    * Constructor.
    */
-  public function __construct(ModuleHandlerInterface $module_handler) {
+  public function __construct(ModuleHandlerInterface $module_handler, RendererInterface $renderer) {
     $this->moduleHandler = $module_handler;
+    $this->renderer = $renderer;
   }
 
   /**
@@ -28,7 +33,8 @@ class ManagePageEditOcr extends FormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('module_handler')
+      $container->get('module_handler'),
+      $container->get('renderer')
     );
   }
 
@@ -57,58 +63,20 @@ class ManagePageEditOcr extends FormBase {
         }
       }
     }
-    // @FIXME
-  // The Assets API has totally changed. CSS, JavaScript, and libraries are now
-  // attached directly to render arrays using the #attached property.
-  //
-  //
-  // @see https://www.drupal.org/node/2169605
-  // @see https://www.drupal.org/node/2408597
-  // drupal_add_library('system', 'ui.dialog');
 
-    $path = drupal_get_path('module', 'islandora_paged_content');
-    // @FIXME
-  // The Assets API has totally changed. CSS, JavaScript, and libraries are now
-  // attached directly to render arrays using the #attached property.
-  //
-  //
-  // @see https://www.drupal.org/node/2169605
-  // @see https://www.drupal.org/node/2408597
-  // drupal_add_js(array('pid' => $pid, 'width' => $width), 'setting');
-
-    // @FIXME
-  // The Assets API has totally changed. CSS, JavaScript, and libraries are now
-  // attached directly to render arrays using the #attached property.
-  //
-  //
-  // @see https://www.drupal.org/node/2169605
-  // @see https://www.drupal.org/node/2408597
-  // drupal_add_js("$path/js/islandora_page_ocr_edit.js");
-
-    // @FIXME
-  // The Assets API has totally changed. CSS, JavaScript, and libraries are now
-  // attached directly to render arrays using the #attached property.
-  //
-  //
-  // @see https://www.drupal.org/node/2169605
-  // @see https://www.drupal.org/node/2408597
-  // drupal_add_css("$path/css/islandora_paged_content.css");
-
-
-    // @FIXME
-  // theme() has been renamed to _theme() and should NEVER be called directly.
-  // Calling _theme() directly can alter the expected output and potentially
-  // introduce security issues (see https://www.drupal.org/node/2195739). You
-  // should use renderable arrays instead.
-  //
-  //
-  // @see https://www.drupal.org/node/2195739
-  // $img = theme('image', array(
-  //     'path' => "islandora/object/$pid/datastream/JPG/view",
-  //     'alt' => $object->label,
-  //     'attributes' => array('id' => 'source_image'),
-  //   ));
-
+    $form['#attached']['drupalSettings']['islandora_paged_content']['edit_ocr'] = [
+      'pid' => $pid,
+      'width' => $width,
+    ];
+    $form['#attached']['library'][] = 'islandora_paged_content/edit-ocr';
+    $img = [
+      '#theme' => 'image',
+      '#uri' => Url::fromRoute('islandora.view_datastream_view', ['object' => $pid, 'datastream' => 'JPG'])->toString(),
+      '#attributes' => [
+        'id' => 'source_image',
+      ],
+    ];
+    $img = $this->renderer->render($img);
     $label = $object->label;
     $prompt = $this->t('Show reference image');
     $form['image'] = [
